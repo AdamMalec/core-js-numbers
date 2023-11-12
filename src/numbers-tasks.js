d18a542a3f6125f1f117a68c5dc457a7c284a9af
Adam Malec <add.malec@gmail.com>
2023-11-12 20:39:04 +0300

    feat: complete getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -248,8 +248,17 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) return 0;
+  if (index === 1) return 1;
+
+  const fibArr = [0, 1, 1];
+
+  for (let i = 2; i < index; i += 1) {
+    fibArr.push(fibArr[i] + fibArr[i - 1]);
+  }
+
+  return fibArr[fibArr.length - 1];
 }
 
 /**
